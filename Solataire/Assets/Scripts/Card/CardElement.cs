ca6d12db28b7e545a252e087d6510bcacc135163
ThanhNguyen <linhmoi95@gmail.com>
2020-03-22 16:38:00 +0700

    - Change texture name to format <Suit>_<Value> for dynamic loading - [WIP] Generate and deal cards

---
+++ Solataire/Assets/Scripts/Card/CardElement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class CardElement : MonoBehaviour
+{
+    [SerializeField]private Sprite m_Front;
+    [SerializeField]private Sprite m_Back;
+    [SerializeField]private SpriteAtlas m_SpriteAtlas;
+    private SpriteRenderer m_Renderer;
+    [SerializeField]private ushort m_CardValue;
+    [SerializeField]private string m_CardName;
+    public bool isFaceUp = false;
+
+    private void Awake()
+    {
+        m_Renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(isFaceUp)
+        {
+            m_Renderer.sprite = m_Front;
+        }
+        else
+        {
+            m_Renderer.sprite = m_Back;
+        }
+    }
+
+    public void SetCardProperties(ushort cardValue, string cardName)
+    {
+        m_CardName = cardName;
+        m_CardValue = cardValue;
+
+        m_Front = m_SpriteAtlas.GetSprite(m_CardName);
+    }
+}
--- Solataire/Assets/Scripts/GameController.cs
+++ Solataire/Assets/Scripts/GameController.cs
@@ -1,17 +1,54 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEngine.U2D;
+
+namespace Solitaire
+{
+    [Flags]
+    public enum CardValue
+    {
+        Ace = 0,
+        Two = 1,
+        Three = 1 << 1,
+        Four = 1 << 2,
+        Five = 1 << 3,
+        Six = 1 << 4,
+        Seven = 1 << 5,
+        Eight = 1 << 6,
+        Nine = 1 << 7,
+        Ten = 1 << 8,
+        Jack = 1 << 9,
+        Queen = 1 << 10,
+        King = 1 << 11
+    }
+
+    [Flags]
+    public enum SuitType
+    {
+        Spades = 1 << 12,
+        Clubs = 1 << 13,
+        Diamonds = 1 << 14,
+        Hearts = 1 << 15
+    }
+}
 
 public class GameController : MonoBehaviour
 {
     [SerializeField]private CommonData m_MainData;
+    [SerializeField]private GameObject m_CardPrefab;
+    [SerializeField]private List<CardElement> m_ListCards;
+    [SerializeField] private GameObject[] m_BottomList;
+    [SerializeField] private GameObject[] m_TopList;
     public List<int> ListCards;
+
 
     // Start is called before the first frame update
     private void Start()
     {
         ListCards = new List<int>();
+        m_ListCards = new List<CardElement>();
         GenerateDeck();
     }
 
@@ -23,13 +60,35 @@ public class GameController : MonoBehaviour
 
     private void GenerateDeck()
     {
+        StringBuilder builder = new StringBuilder();
+        ushort suitOffset = 12;
+        ushort cardOffset = 1;
+
+        float xOffset = 0.0f;
+        float yOffset = 0.0f;
+
         for(int i = 0; i < 4; ++i)
         {
             for(int k = 0; k < 13; ++k)
             {
-                ListCards.Add(0 | m_MainData.suits[i] | m_MainData.cardValues[k]);
+                ushort suitValue = (ushort)(1 << (i + suitOffset));
+                ushort cardValue = (ushort)((k == 0 ? 0 : 1) << (k - cardOffset));
+                builder.Append(Enum.GetName(typeof(Solitaire.SuitType), suitValue));
+                builder.Append("_");
+                builder.Append(Enum.GetName(typeof(Solitaire.CardValue), cardValue));
+                Debug.Log("cardName = " + builder);
+                //ListCards.Add(0 | suitValue | cardValue);
 
-                Debug.Log("Card = " + ListCards[i] + " - Binary = " + Convert.ToString(ListCards[i], 2));
+                //CardElement card = new CardElement();
+                GameObject tempCard = Instantiate(m_CardPrefab, new Vector3(this.transform.position.x, this.transform.position.y - yOffset, this.transform.position.z), Quaternion.identity);
+
+
+                m_ListCards.Add(tempCard.GetComponent<CardElement>());
+                m_ListCards[i * 13 + k].SetCardProperties((ushort)(0 | suitValue | cardValue), builder.ToString());
+
+                yOffset += 0.5f;
+                //Debug.Log("Card = " + ListCards[i] + " - Binary = " + Convert.ToString(ListCards[i], 2));
+                builder.Clear();
             }
         }
     }
