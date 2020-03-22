a049cd361d44c94727ef8e656a3477486dad5bd8
ThanhNguyen <linhmoi95@gmail.com>
2020-03-22 13:45:28 +0700

    + Add Prefab for Card object + Add GameController + Add CommonData asset (contain all the data)

---
+++ Solataire/Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]private CommonData m_MainData;
+    public List<int> ListCards;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ListCards = new List<int>();
+        GenerateDeck();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void GenerateDeck()
+    {
+        for(int i = 0; i < 4; ++i)
+        {
+            for(int k = 0; k < 13; ++k)
+            {
+                ListCards.Add(0 | m_MainData.suits[i] | m_MainData.cardValues[k]);
+
+                Debug.Log("Card = " + ListCards[i] + " - Binary = " + Convert.ToString(ListCards[i], 2));
+            }
+        }
+    }
+}
---
+++ Solataire/Assets/Scripts/Template/CommonData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="CommonData", menuName ="Template/CommonData")]
+public class CommonData : ScriptableObject
+{
+    public ushort[] cardValues = { 0, 1, 1 << 1, 1 << 2, 1 << 3, 1 << 4, 1 << 5, 1 << 6, 1 << 7, 1 << 8,1 << 9, 1 << 10, 1 << 11 };
+    public ushort[] suits = { 1 << 12, 1 << 13, 1 << 14, 1 << 15};
+}
