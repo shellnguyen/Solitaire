9157718dbc6a1974bd8805e0325fe3ef76aa486e
ThanhNguyen <linhmoi95@gmail.com>
2020-04-06 17:09:31 +0700

    - [WIP] Making UIScene a seperate Scene and load it into GameScene at the start

--- Solataire/Assets/Scripts/Menus/MenuController.cs
+++ Solataire/Assets/Scripts/Menus/MenuController.cs
@@ -5,23 +5,23 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    [SerializeField] private GameData m_GameData;
+    //[SerializeField] private GameData m_GameData;
 
-    //TopBar
-    [SerializeField] private RawImage m_GameIcon;
-    [SerializeField] private TextMesh m_GameText;
-    [SerializeField] private TextMesh m_DifficultyText;
-    [SerializeField] private TextMesh m_ScoreText;
-    [SerializeField] private TextMesh m_TimeText;
-    [SerializeField] private TextMesh m_MoveText;
+    ////TopBar
+    //[SerializeField] private RawImage m_GameIcon;
+    //[SerializeField] private TextMesh m_GameText;
+    //[SerializeField] private TextMesh m_DifficultyText;
+    //[SerializeField] private TextMesh m_ScoreText;
+    //[SerializeField] private TextMesh m_TimeText;
+    //[SerializeField] private TextMesh m_MoveText;
 
-    //BottomBar
-    [SerializeField] private Button m_BtnNew;
-    [SerializeField] private Button m_BtnOption;
-    [SerializeField] private Button m_BtnCards;
-    [SerializeField] private Button m_BtnHint;
-    [SerializeField] private Button m_BtnUndo;
-    [SerializeField] private Button m_BtnExit;
+    ////BottomBar
+    //[SerializeField] private Button m_BtnNew;
+    //[SerializeField] private Button m_BtnOption;
+    //[SerializeField] private Button m_BtnCards;
+    //[SerializeField] private Button m_BtnHint;
+    //[SerializeField] private Button m_BtnUndo;
+    //[SerializeField] private Button m_BtnExit;
 
     // Start is called before the first frame update
     private void Start()
---
+++ Solataire/Assets/Scripts/Menus/MenuLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if(!SceneManager.GetSceneByBuildIndex(2).isLoaded)
+        {
+            SceneManager.LoadScene(2, LoadSceneMode.Additive);
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
