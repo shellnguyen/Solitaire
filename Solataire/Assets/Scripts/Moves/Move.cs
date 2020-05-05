3992e660bb4dc5169283aeaf53e4e17180526148
ThanhNguyen <linhmoi95@gmail.com>
2020-05-05 16:48:58 +0700

    + [WIP] Undo feature

---
+++ Solataire/Assets/Scripts/Moves/Move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move
+{
+    private List<CardElement> m_DeckCards;
+    private List<CardElement> m_BottomCards;
+    private List<CardElement> m_TopCards;
+
+    private sbyte m_CurrentDrawCard;
+    private ushort m_Score;
+    private ushort m_MoveNumber;
+
+    //New move hold the current GameData state
+    public Move(GameData data)
+    {
+        m_DeckCards = data.deckCards;
+        m_BottomCards = data.bottomCards;
+        m_TopCards = data.topCards;
+
+        m_CurrentDrawCard = data.currentDrawCard;
+        m_Score = data.score;
+        m_MoveNumber = data.move;
+    }
+
+    //Set GameData state to same as Move
+    public void Execute(GameData data)
+    {
+        data.deckCards = m_DeckCards;
+        data.bottomCards = m_BottomCards;
+        data.topCards = m_TopCards;
+
+        data.currentDrawCard = m_CurrentDrawCard;
+        data.score = m_Score;
+        data.move = m_MoveNumber;
+    }
+}
---
+++ Solataire/Assets/Scripts/Moves/MovesManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovesManager
+{
+    private Stack<Move> m_Moves;
+
+    public MovesManager()
+    {
+        m_Moves = new Stack<Move>();
+    }
+
+    public void AddMove(Move move)
+    {
+        m_Moves.Push(move);
+    }
+
+    public void Undo(GameData data)
+    {
+        m_Moves.Pop();
+        m_Moves.Peek().Execute(data);
+    }
+
+    public bool CanUndo()
+    {
+        if(m_Moves.Count > 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
