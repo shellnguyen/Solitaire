1c2ca744b8149ece953eb96106fd42267c05572d
ThanhNguyen <linhmoi95@gmail.com>
2020-05-09 15:52:32 +0700

    [WIP] Show Ads - Add Admod SDK for Unity (FUCK YOU GOOGLE, YOU MESSED UP MY PROJECT SETUP @&#@!#@##$&^#@)

---
+++ Solataire/Assets/Scripts/Test/TestScripts.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScripts : MonoBehaviour
+{
+    public TestCard card;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(mousePos), Vector2.zero);
+
+            if(hit)
+            {
+                card.Move(hit.collider.gameObject);
+            }
+
+        }
+    }
+}
