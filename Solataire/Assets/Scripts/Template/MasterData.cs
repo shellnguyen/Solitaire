ff17c54d6fbcb4db4c8f09036ad853e54859dfcd
ThanhNguyen <linhmoi95@gmail.com>
2020-05-11 17:54:42 +0700

    + Added MasterData.asset for Android

--- Solataire/Assets/Scripts/Template/MasterData.cs
+++ Solataire/Assets/Scripts/Template/MasterData.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="MasterData", menuName ="Template/MasterData")]
 public class MasterData : ScriptableSingleton<MasterData>
 {
+    //Admod
+    public string Admod_AppId;
+    public string Admod_AdUnit_Banner_Id;
+    public string Admod_AdUnit_Banner_Test_Id;
+    //
+
+    //UnityAds
+    public string UnityAds_AppId;
+    //
 }
