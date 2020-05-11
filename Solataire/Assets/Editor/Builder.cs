c823138091463b80e7bf4eb7cf145778d082b898
ThanhNguyen <linhmoi95@gmail.com>
2020-05-11 16:21:22 +0700

    + Added a simple build script to build difference build with specific flags + Added MasterData.cs (SO).

---
+++ Solataire/Assets/Editor/Builder.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class Builder
+{
+    public static string[] scenes = { "Assets/Scenes/LoadingScene.unity", "Assets/Scenes/StartScene.unity", "Assets/Scenes/MainScene.unity", "Assets/Scenes/UIScene.unity" };
+    public const string RELEASE_FLAG = "RELEASE_BUILD";
+    public const string DEV_FLAG = "DEV_BUILD";
+
+    private static void AddFlag(string flag)
+    {
+        // Get defines.
+        BuildTargetGroup buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+        string defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
+
+        // Append only if not defined already.
+        if (defines.Contains(flag))
+        {
+            Debug.LogWarning("Selected build target (" + EditorUserBuildSettings.activeBuildTarget.ToString() + ") already contains <b>" + flag + "</b> <i>Scripting Define Symbol</i>.");
+            return;
+        }
+
+        // Append.
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, (defines + ";" + flag));
+        Debug.LogWarning("<b>" + flag + "</b> added to <i>Scripting Define Symbols</i> for selected build target (" + EditorUserBuildSettings.activeBuildTarget.ToString() + ").");
+    }
+
+    public static void AndroidRelease()
+    {
+        AddFlag(RELEASE_FLAG);
+
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = scenes;
+        options.target = BuildTarget.Android;
+        options.locationPathName = "../build/AndroidRelease";
+        options.options = BuildOptions.CompressWithLz4HC;
+        EditorUserBuildSettings.buildAppBundle = true;
+
+        BuildReport report = BuildPipeline.BuildPlayer(options);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build AndroidRelease succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build AndroidRelease failed");
+        }
+    }
+
+    public static void AndroidDev()
+    {
+        AddFlag(DEV_FLAG);
+
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = scenes;
+        options.target = BuildTarget.Android;
+        options.locationPathName = "../build/AndroidDev";
+        options.options = BuildOptions.CompressWithLz4HC | BuildOptions.Development | BuildOptions.AllowDebugging;
+
+        BuildReport report = BuildPipeline.BuildPlayer(options);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build AndroidDev succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build AndroidDev failed");
+        }
+    }
+
+    public static void WebGLDev()
+    {
+        AddFlag(DEV_FLAG);
+
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = scenes;
+        options.target = BuildTarget.WebGL;
+        options.locationPathName = "../build/WebGLDev";
+        options.options = BuildOptions.Development;
+
+        BuildReport report = BuildPipeline.BuildPlayer(options);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build WebGLDev succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build WebGLDev failed");
+        }
+    }
+
+    public static void WebGLRelease()
+    {
+        AddFlag(RELEASE_FLAG);
+
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = scenes;
+        options.target = BuildTarget.WebGL;
+        options.locationPathName = "../build/WebGLRelease";
+        options.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(options);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build WebGLRelease succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build WebGLRelease failed");
+        }
+    }
+
+    public static void IosRelease()
+    {
+        AddFlag(RELEASE_FLAG);
+
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = scenes;
+        options.target = BuildTarget.Android;
+        options.locationPathName = "../build/IOSRelease";
+        options.options = BuildOptions.CompressWithLz4HC;
+        EditorUserBuildSettings.iOSBuildConfigType = iOSBuildType.Release;
+
+        BuildReport report = BuildPipeline.BuildPlayer(options);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build IOSRelease succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build IOSRelease failed");
+        }
+    }
+
+    public static void IosDev()
+    {
+        AddFlag(DEV_FLAG);
+
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = scenes;
+        options.target = BuildTarget.iOS;
+        options.locationPathName = "../build/IOSDev";
+        options.options = BuildOptions.Development | BuildOptions.AllowDebugging;
+        EditorUserBuildSettings.iOSBuildConfigType = iOSBuildType.Debug;
+
+        BuildReport report = BuildPipeline.BuildPlayer(options);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build IOSDev succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build IOSDev failed");
+        }
+    }
+
+    public static void WindowsRelease()
+    {
+        AddFlag(RELEASE_FLAG);
+
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = scenes;
+        options.target = BuildTarget.StandaloneWindows64;
+        options.locationPathName = "../build/WindowsRelease";
+        options.options = BuildOptions.CompressWithLz4HC;
+
+        BuildReport report = BuildPipeline.BuildPlayer(options);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build WindowsRelease succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build WindowsRelease failed");
+        }
+    }
+
+    public static void WindowsDev()
+    {
+        AddFlag(DEV_FLAG);
+
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = scenes;
+        options.target = BuildTarget.StandaloneWindows64;
+        options.locationPathName = "../build/WindowsDev";
+        options.options = BuildOptions.Development | BuildOptions.AllowDebugging;
+
+        BuildReport report = BuildPipeline.BuildPlayer(options);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build WindowsDev succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build WindowsDev failed");
+        }
+    }
+}
