404372f49f2feb2299009102cdf9164992b6e39e
ThanhNguyen <linhmoi95@gmail.com>
2020-05-14 17:04:16 +0700

    + Added HowToPlay popup + Added ScrollPanel element

---
+++ Solataire/Assets/Scripts/Menus/Shared/Popup/HowToPlayPopup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToPlayPopup : Popup
+{
+    [SerializeField] private ScrollController m_ScrollPanel;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    public void SetData()
+    {
+        m_ScrollPanel.SetData(ResourcesManager.Instance.HintTexts);
+    }
+}
