0e99af3d21786a80d26c4560c4e464d1d5957c0b
ThanhNguyen <linhmoi95@gmail.com>
2020-05-18 13:35:45 +0700

    - Remove unused code

--- Solataire/Assets/Scripts/Menus/Shared/FlexiableGridLayout.cs
+++ Solataire/Assets/Scripts/Menus/Shared/FlexiableGridLayout.cs
@@ -85,16 +85,4 @@ public class FlexiableGridLayout : LayoutGroup
     {
         
     }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        
-    }
 }
